30c3cae78ea327f88c140c33262be6095aa652bb
enricote <enrico.carvalho@unesp.br>
2022-09-23 13:41:29 -0300

    teste
    dsfasd

--- coisasGit/BancoV6/index.php
+++ coisasGit/BancoV6/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../BancoV6/css/cabecalho/style.css">
     <script src="https://kit.fontawesome.com/0dcacea947.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="../BancoV6/imagem/logopreta.png" />
     <title>Home</title>
 </head>
 
@@ -40,72 +41,82 @@
             </div>
             <div class='carrinho'>
                 <div class='carrinho_icone'>
-                   <a href="#"> <i class="fa-thin fa-cart-shopping fa"></i></a>
+                    <a href="#"> <i class="fa-thin fa-cart-shopping fa"></i></a>
                 </div>
             </div>
         </div>
-        <a href='Cadastros/Produtos/cadastros/cad_pesq_produtos_front.php' target='_parent'>Produtos</a>
+        <!-- <a href='Cadastros/Produtos/cadastros/cad_pesq_produtos_front.php' target='_parent'>Produtos</a>
         <a href='Cadastros/Usuarios/cadastros/cad_pesq_usuarios_front.php' target='_parent'>Usuários</a>
         <a href='Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php' target='_parent'>Seleção de Produtos</a>
-        <br><br><br><br>
+-->
         <div class="corpo">
+            <a name="topo"></a>
             <div class="descricao">
-                <div class="desc_txt">
-                    <p class="desc_txt1">Pic roll tem como foco as memórias tomando formas de registros, para que assim o momento fique marcado de forma fisica e memorável.</p>
-                </div>
                 <div class="desc_img1">
-                    <img class="desc_img2" src="../BancoV6/imagem/logopreta.png">
+                    <img class="desc_img2" src="../BancoV6/imagem/fotoprinci.png">
+                    <h1 class="desc_txt2">Guarde suas lembranças</h1>
+                    <p class="desc_txt1">Pic roll tem como foco as memórias tomando formas de registros, para que assim o momento fique marcado de forma fisica e memorável.</p>
+                    <div class="desc_btn"><a href="../BancoV6/Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php">Nossos produtos</a></div>
                 </div>
             </div>
-            <br><br><br><br>
+            <br>
             <div class="prod_imgs">
 
                 <div class="prod1">
-                    <img src="../BancoV6/imagem/logopreta.png" class="prod_img">
+                    <div class="prod_img1">
+                    <img src="../BancoV6/imagem/imagemba2.jpeg" class="prod_img">
+                    </div>
                     <div class="prod_txt1">
                         <p class="prod_txt">
-                            Totem para tirar suas fotos.
+                            O totem responsável por registrar momentos de forma fisica.
                         </p>
+                        <div class="prod_btn1"><a href="../BancoV6/Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php">Comprar</a></div>
                     </div>
-                    <br><br>
                 </div>
                 <div class="prod2">
-                    <img src="../BancoV6/imagem/logopreta.png" class="prod_img">
+                <div class="prod_img1">
+                    <img src="../BancoV6/imagem/imagemba1.jpeg" class="prod_img">
+                </div>
                     <div class="prod_txt1">
                         <p class="prod_txt">
-                            Tire você, suas próprias fotos !
+                            Tire foto com amigos para que possam se lembrar do dia.
                         </p>
+                    <div class="prod_btn1"><a href="../BancoV6/Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php">Comprar</a></div>
                     </div>
                 </div>
                 <div class="prod3">
-                    <img src="../BancoV6/imagem/logopreta.png" class="prod_img">
+                <div class="prod_img1">
+                    <img src="../BancoV6/imagem/imagemba3.jpg" class="prod_img">
+                </div>
                     <div class="prod_txt1">
                         <p class="prod_txt">
-                            Seus momentos ficarão assim.
+                            E por fim tenha registrado de forma fisica.
                         </p>
+                        <div class="prod_btn1"><a href="../BancoV6/Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php">Comprar</a></div>
                     </div>
                 </div>
             </div>
-
+            <br><br>
             <div class="prod_video">
                 <div class="prod_vidt">
+                    <h1 class="vidtxt1">Nosso produto</h1>
                     <p class="vidtxt">
-                        Vídeo explicativo de como serão produzidas as suas fotos, e como serão divertidos esses momentos.
+                        Vídeo explicando o processo da captura com detalhes dos produtos.
                     </p>
                 </div>
                 <div class="prod_vidv">
-                    <iframe class="vidv" src="https://www.youtube.com/embed/VZne39DFitM" title="YouTube video player" allow="accelerometer; autoplay;
-                 clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe class="vidv" src="https://www.youtube.com/embed/l7lHlDdTQx4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
 
-
-
-            <br><br><br><br>
+            <br>
             <footer class="footer">
                 <div class="container">
                     <div class="row">
-                        <div class="footer-col">
+                        <div class="footer_imgc">
+                            <img class="footer_img" src="../BancoV6/imagem/logoembranco.png" alt="Nossa Marca">
+                        </div>
+                        <div class="footer-col1">
                             <h4>Navegação</h4>
                             <ul>
                                 <li><a href="../BancoV6/index.php">Home</a></li>
@@ -115,9 +126,7 @@
                                 <li><a href="#">Patrocinadores</a></li>
                             </ul>
                         </div>
-                        <div class="footer_imgc">
-                            <img class="footer_img" src="../BancoV6/imagem/logoembranco.png" alt="Nossa Marca">
-                        </div>
+
                         <div class="footer-col">
                             <h4>Desenvolvedores</h4>
                             <ul>
@@ -131,14 +140,15 @@
                         <div class="footer-col">
                             <h4>Contate-nos</h4>
                             <div class="social">
-                                    <a href="https://instagram.com/picrollbauru?igshid=YmMyMTA2M2Y="><i class="fa-brands fa-instagram fa"></i></a>
-                                </div>
+                                <a href="https://instagram.com/picrollbauru?igshid=YmMyMTA2M2Y="><i class="fa-brands fa-instagram fa"></i></a>
+                            </div>
                             <ul>
                                 <li><a href="https://goo.gl/maps/NAnKGTvv8a1h7rFS9">Rua Dos Andradas 3-17,Bauru 17050-200, Brasil</a></li>
-
+                                <br>
                                 <li><a class="footer_email" href="mailto:picrollbauru@gmail.com">Nos mande um email</a></li>
-                                
+
                         </div>
+                        <div class="vtop"><a href="#topo"><i class="fa-thin fa-arrow-up fa"></i></a></div>
                     </div>
                 </div>
             </footer>
